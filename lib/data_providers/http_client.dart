7ff0a756e31c0811ddc90f50ba0d41adca3da640
Majid <mhadaily@gmail.com>
2021-02-20 13:04:56 +0100

    replace sample network to reqres

--- lib/data_providers/http_client.dart
+++ lib/data_providers/http_client.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 class HttpClient {
   Future<http.Response> get(String api) async {
     // just to fake an API request
-    await http.get('https://dummyapi.io/data/api/user?limit=10');
+    await http.get('https://reqres.in/api/users');
     return Future.delayed(
       const Duration(seconds: 1),
       () => http.Response('success', 200),
@@ -12,7 +12,7 @@ class HttpClient {
 
   Future<http.Response> post(String api, String body) async {
     // just to fake an API request
-    await http.get('https://dummyapi.io/data/api/user?limit=10');
+    await http.get('https://reqres.in/api/users');
     return Future.delayed(
       const Duration(seconds: 1),
       () => http.Response('success', 200),
